50e4c5357f7d9256ffa17a81ec98a51f68ae986e
alper <alperaltuntas@gmail.com>
2015-05-20 15:07:33 -0400

    comment

--- SMT/Project/Files/Maxele63.h
+++ SMT/Project/Files/Maxele63.h
@@ -2,6 +2,12 @@
 #define MAXELE63_H
 
 #include <QObject>
+#include <QString>
+
+#include <vector>
+#include <istream>
+#include <fstream>
+#include <sstream>
 
 #include "Project/Files/ProjectFile.h"
 
@@ -13,10 +19,22 @@ class Maxele63 : public QObject
 		Maxele63(ProjectFile *projectFile, QObject *parent=0);
 		Maxele63(QString domainName, ProjectFile *projectFile, QObject *parent=0);
 
+        QString                 GetHeaderLine();
+        unsigned int            GetNumNodes();
+        std::vector<float>      GetMaxeles();
+
+
 	private:
 
-		QString			domainName;
-		ProjectFile*	projectFile;
+        QString             domainName;
+        ProjectFile*        projectFile;
+        bool                isFullDomain;
+
+        QString             headerLine;
+        unsigned int        numNodes;
+        std::vector<float>  maxeles;
+
+        void ReadFile();
 };
 
 #endif // MAXELE63_H
